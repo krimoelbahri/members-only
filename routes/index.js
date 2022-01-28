35c2115d8b6bf3097e4cd11e32d7b3430e40c1c5
Karim <k.guezlane@gmail.com>
2022-01-28 23:19:56 +0100

    handeling requests

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,14 @@ const indexController = require("../controllers/indexController");
 
 /* GET home page. */
 router.get("/", indexController.home);
-
+/* GET login page. */
+router.get("/login", indexController.login);
+/* POST req login */
+router.post("/login", indexController.login_post);
+/* GET signup page. */
+router.get("/signup", indexController.signup);
+/* POST req signup */
+router.post("/signup", indexController.signup_post);
+/* GET logout page. */
+router.get("/logout", indexController.logout);
 module.exports = router;
