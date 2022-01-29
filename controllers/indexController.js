7543dcfc519a127eb38f3292a94cbed88afb82ab
Karim <k.guezlane@gmail.com>
2022-01-29 12:16:55 +0100

    added controllers

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,25 +2,59 @@ const bcrypt = require("bcryptjs");
 const { body, validationResult } = require("express-validator");
 const passport = require("passport");
 const User = require("../models/user");
+const Message = require("../models/message");
 
+/*------------------------- GET controllers ----------- */
 exports.home = function (req, res, next) {
-	res.render("index", { title: "Elbahri club", messages_list: [] });
+	Message.find()
+		.populate("author")
+		.sort([["date", "ascending"]])
+		.exec(function (err, message_list) {
+			if (err) {
+				return next(err);
+			}
+			res.render("index", { title: "Elbahri club", message_list });
+		});
 };
-
+exports.member = function (req, res) {
+	if (!req.user || req.user.membership != "user") {
+		res.redirect("/");
+	}
+	res.render("member", { title: "Become a Member" });
+};
+exports.admin = function (req, res) {
+	if (!req.user || req.user.membership != "member") {
+		res.redirect("/");
+	}
+	res.render("admin", { title: "Become an Admin" });
+};
+exports.addMessage = function (req, res) {
+	if (!req.user) {
+		res.redirect("/");
+	}
+	res.render("message", { title: "Add a Message" });
+};
 exports.login = function (req, res) {
 	if (req.user) {
 		res.redirect("/");
 	}
 	res.render("login", { title: "login" });
 };
-
 exports.signup = function (req, res) {
 	if (req.user) {
 		res.redirect("/");
 	}
 	res.render("signup", { title: "signup", errors: [] });
 };
+exports.delete = function (req, res, next) {
+	res.render("admin", { title: "Elbahri club" });
+};
+exports.logout = function (req, res) {
+	req.logout();
+	res.redirect("/");
+};
 
+/*----------------POST controllers -------------------*/
 exports.login_post = function (req, res, next) {
 	passport.authenticate("local", function (err, user, info) {
 		if (err) {
@@ -58,7 +92,7 @@ exports.signup_post = [
 	function (req, res, next) {
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
-			res.render("signup", { title: "login", errors: errors.errors });
+			res.render("signup", { title: "signup", errors: errors.errors });
 		} else {
 			bcrypt.hash(req.body.password, 15, (err, hashedPassword) => {
 				// if err, do something
@@ -81,7 +115,38 @@ exports.signup_post = [
 	},
 ];
 
-exports.logout = function (req, res) {
-	req.logout();
-	res.redirect("/");
+exports.addMessage_post = [
+	body("title")
+		.trim()
+		.isAlphanumeric("en-US", { ignore: " -" })
+		.withMessage("Title must have alphabitical or numurical values only ")
+		.isLength({ min: 15 })
+		.withMessage("Title must at least have 15 characters"),
+	body("message").isLength({ min: 30 }).withMessage("message should atleast have 30 characters"),
+	function (req, res, next) {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			console.log(errors.errors);
+			res.render("message", { title: "Add a Message", errors: errors.errors });
+		} else {
+			const message = new Message({
+				title: req.body.title,
+				body: req.body.message,
+				author: req.user._id,
+			}).save((err) => {
+				if (err) {
+					return next(err);
+				}
+				res.redirect("/");
+			});
+		}
+	},
+];
+
+exports.admin_post = function (req, res, next) {
+	res.render("admin", { title: "Elbahri club" });
+};
+
+exports.member_post = function (req, res, next) {
+	res.render("admin", { title: "Elbahri club" });
 };
